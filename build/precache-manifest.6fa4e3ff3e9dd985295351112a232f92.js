909930819493845ba7959ed162969217379d749f
Govind B Chandran <govindchandran235@gmail.com>
2020-10-07 03:38:25 +0530

    App Completed

---
+++ build/precache-manifest.6fa4e3ff3e9dd985295351112a232f92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "315c278999cdce59d93f288953143f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "580d31aff5f4f655032a",
+    "url": "/static/css/main.74f66ef6.chunk.css"
+  },
+  {
+    "revision": "30febc70bc384999498a",
+    "url": "/static/js/2.7b4c29d0.chunk.js"
+  },
+  {
+    "revision": "4cd4bb82c3efd9af5440b12fb6d92f3f",
+    "url": "/static/js/2.7b4c29d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "580d31aff5f4f655032a",
+    "url": "/static/js/main.6a18039c.chunk.js"
+  },
+  {
+    "revision": "60889e9a4e7f2aebb705",
+    "url": "/static/js/runtime-main.9690a2d3.js"
+  }
+]);
